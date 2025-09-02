55b55a69fae099119b13e6594e5401bc03dbac83
ravargas <ravargas.42t@gmail.com>
2025-09-02 09:00:48 +0200

    first commit

---
+++ include/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ravargas <ravargas@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 08:46:41 by ravargas          #+#    #+#             */
+/*   Updated: 2025/09/02 08:53:16 by ravargas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+//TODO: complete lib includes
+
+//TODO: build program data structure
+
+#endif
